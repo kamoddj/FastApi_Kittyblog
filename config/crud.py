51d1d07fbca2382bea382dad2e608bb6e479ddf1
Сергей Погуляйло <ya.kamod123@gmail.com>
2023-11-10 18:02:23 +0300

    refact

--- config/crud.py
+++ config/crud.py
@@ -1,5 +1,10 @@
+from datetime import datetime, timedelta
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from config.exceptions import UNAUTORIZED_EXCEPTION_401
 from config.hashing import Hasher
+from models.database import get_db
 from models.models import User
 from models.shcemas import UserCreate
 from sqlalchemy.orm import Session
@@ -14,6 +19,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
 
 
+# FastApi /users
 def create_new_user(user: UserCreate, db: Session):
     user_model = User(
         name=user.name.title(),
@@ -27,19 +33,38 @@ def get_user_by_email(email: str, db: Session):
     user = db.query(User).filter(User.email == email).first()
     return user
 
-# def authenticate_user(email: str, password: str, db: Session):
-#     user = db.query(User).filter(User.email == email).first()
-#     if not Hasher.verify_password(password, user.hashed_password):
-#         return False
-#     return User
+
+# FastApi /login
+def authenticate_user(email: str, password: str, db: Session):
+    user = get_user_by_email(email=email, db=db)
+    if not user:
+        return False
+    if not Hasher.verify_password(password, user.hashed_password):
+        return False
+    return user
 
 
-# def create_access_token(data: dict, expires_delta: timedelta | None = None):
-#     to_encode = data.copy()
-#     if expires_delta:
-#         expire = datetime.utcnow() + expires_delta
-#     else:
-#         expire = datetime.utcnow() + datetime.timedelta(minutes=15)
-#     to_encode.update({"exp": expire})
-#     encoded_jwt = JWTError.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-#     return encoded_jwt
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_scheme),
+                     db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise UNAUTORIZED_EXCEPTION_401
+    except JWTError:
+        raise UNAUTORIZED_EXCEPTION_401
+    user = get_user_by_email(email=email, db=db)
+    if user is None:
+        raise UNAUTORIZED_EXCEPTION_401
+    return user
